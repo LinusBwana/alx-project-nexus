00d6925684c6705062e28de20be3c969023b8203
LinusBwana <linusbwana49@gmail.com>
2025-11-15 23:37:06 +0300

    feat(auth): implement user registration and JWT login with refresh and access tokens

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,9 +11,17 @@ class ProfileInline(admin.StackedInline):
 
 # Custom admin for our custom User model
 class UserAdmin(BaseUserAdmin):
-    # Attach the profile inline so User + Profile appear together
-    inlines = [ProfileInline]
-     # Make auto-generated fields read-only instead of editable
+    inlines = [ProfileInline] # Attach the profile inline so User + Profile appear together
+
+    # Adding your custom fields
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
+        ('Role & Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ['username', 'first_name', 'last_name', 'role', 'is_active', 'is_staff']
+    # Make auto-generated fields read-only instead of editable
     readonly_fields = ["date_joined", "last_login"]
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, mixins
+from .models import User
+from .serializers import UserSerializer, RegisterUserSerializer, LoginSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+
+
+class LoginViewSet(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        """
+        Handle user login (POST request)
+        """
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # Valid login, returning access and refresh tokens
+            user = serializer.validated_data['user']
+            user_data = UserSerializer(user).data
+            return Response({
+                'refresh': serializer.validated_data['refresh'],
+                'access': serializer.validated_data['access'],
+                'user': user_data
+            }, status=status.HTTP_200_OK)
+
+        # If invalid login credentials
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
